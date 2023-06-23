380644b7e6ff54c6867af5c869cac9fba093589e
Kevin Davis <kevincarrolldavis@gmail.com>
2023-06-23 14:33:38 -0400

    Steps

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
+import os
+import subprocess
+from pathlib import Path
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup, find_packages
+
+build_dir = Path("build")
+build_dir.mkdir(exist_ok=True)
+# configure
+subprocess.run(
+    ["cmake", ".."],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    cwd="build",
+)
+subprocess.run(
+    ["cmake", "--build", "."],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    cwd="build",
+)
 
 setup(
     ext_modules=[
@@ -8,10 +29,15 @@ setup(
             [
                 "src/add.cpp",
                 "src/entity.cpp",
-                "src/ext.cpp"
+                "src/ext.cpp",
+                "src/session.cpp",
+                "src/subject.cpp",
             ],
-            include_dirs=["include"],
+            include_dirs=[
+                "include",
+                build_dir / "json" / "include"
+            ],
         )
     ],
-    cmdclass={"build_ext": build_ext}
+    cmdclass={"build_ext": build_ext},
 )
--- src/ext.cpp
+++ src/ext.cpp
@@ -2,8 +2,12 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
+#include <memory>
+
 #include "add.hpp"
 #include "entity.hpp"
+#include "session.hpp"
+#include "subject.hpp"
 
 namespace py = pybind11;
 
@@ -23,4 +27,11 @@ PYBIND11_MODULE(clibbids, m) {
           py::arg("name"), py::arg("key") = py::none(), py::arg("value") = "0",
           py::arg("padding") = 2)
       .def_property_readonly("id", &Entity::id);
+
+  py::class_<Session>(m, "Session")
+      .def(py::init([](py::object sub, int i) {
+        return Session(*py::cast<Subject*>(sub), i);
+      }))
+      .def_property_readonly("path", &Session::path)
+      .def_property_readonly("prefix", &Session::prefix);
 }
