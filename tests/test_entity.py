89b14c94948b1c1be11bfa8191a592386f36eb80
Kevin Davis <kevincarrolldavis@gmail.com>
2023-06-27 00:50:17 -0400

    Testing entity

---
+++ tests/test_entity.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from libbids.clibbids import Entity
+
+
+def test_constructor_with_name_and_optional_key_and_int_value():
+    name = "Entity"
+    key: Optional[str] = "KEY"
+    value: int = 42
+    padding: int = 2
+
+    entity = Entity(name, key, value, padding)
+
+    assert entity.id == "KEY-42"
+
+def test_constructor_with_name_and_optional_key_and_string_value():
+    name = "Entity"
+    key: Optional[str] = "KEY"
+    value: str = "VALUE"
+    padding: int = 2
+
+    entity = Entity(name, key, value, padding)
+
+    assert entity.id == "KEY-VALUE"
+
+def test_constructor_with_name_and_optional_key_and_invalid_string_value():
+    name: str = "Entity"
+    key: Optional[str] = "KEY"
+    value: str = "INVALID"
+    padding: int = 2
+
+    entity: Entity = Entity(name, key, value, padding)
+
+    assert entity.id == "KEY-INVALID"
+    assert entity.padding == 2
