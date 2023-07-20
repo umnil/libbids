380944da10d1a0d5d74d1c649f25b53704b43d3f
Kevin Davis <kevincarrolldavis@gmail.com>
2023-07-20 11:22:00 -0400

    Include vulkan

--- scripts/linux/install-dependencies.sh
+++ scripts/linux/install-dependencies.sh
@@ -8,6 +8,9 @@ apt install libglx-dev -y
 apt install qt6-base-dev -y
 
 echo "apt-get"
+add-apt-repository ppa:oibaf/graphics-drivers
 apt-get update 
+apt-get upgrade
 apt-get install -y libgl1-mesa-dev
 apt-get install -y libxkbcommon-dev
+apt-get install -y libvulkan1 mesa-vulkan-drivers vulkan-utils
