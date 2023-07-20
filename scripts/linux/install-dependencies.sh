012a291f181ddcd03c9fe6ea5ec69ac73b3c0747
Kevin Davis <kevincarrolldavis@gmail.com>
2023-07-20 10:27:58 -0400

    Apt and apt-get

--- scripts/linux/install-dependencies.sh
+++ scripts/linux/install-dependencies.sh
@@ -1,5 +1,12 @@
 echo "Install Linux Dependencies"
+
+echo "apt"
 apt update
+
 apt install libegl1 -y
 apt install libglx-dev -y
 apt install qt6-base-dev -y
+
+echo "apt-get"
+apt-get update 
+apt-get install -y libgl1-mesa-dev
